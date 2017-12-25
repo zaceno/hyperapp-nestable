639b7fba88a4079fa829a55031f12b972141f95e
Zacharias Enochsson <zache@axis.com>
2017-12-25 22:45:07 +0100

    fix h dep

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 export default function (state, actions, view, tagname) {
-  actions._$ = function (x) { return x }
-  return function (props) {
-      return h(tagname || 'x-', {
-          oncreate: function (el) {
-              var _actions = app(state, actions, view, el)
-              el._$ = _actions._$
-              el._$(props)
-              if (_actions.init) _actions.init()
-              el._$$ = _actions.uninit
-          },
-          onupdate: function (el) {
-              el._$(props)
-          },
-          onremove: function (el) {
-              if (el._$$) el._$$()
-          }
-      })
-  }
+    actions._$ = function (x) { return x }
+    return function (props) {
+        return {
+            type: tagname || 'x-',
+            props: {
+                oncreate: function (el) {
+                    var _actions = app(state, actions, view, el)
+                    el._$ = _actions._$
+                    el._$(props)
+                    if (_actions.init) _actions.init()
+                    el._$$ = _actions.uninit
+                },
+                onupdate: function (el) {
+                    el._$(props)
+                },
+                onremove: function (el) {
+                    if (el._$$) el._$$()
+                }
+            },
+            children: []
+        }
+    }
 }
