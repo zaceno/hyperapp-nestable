fd0ab8dcecb70f080ced0c5eb8b142cb8454b2d2
Zacharias Enochsson <zache@axis.com>
2017-12-25 22:40:41 +0100

    fix nbsp typo

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ export default function (state, actions, view, tagname) {
   actions._$ = function (x) { return x }
   return function (props) {
       return h(tagname || 'x-', {
-          oncreate: function (el) {
+          oncreate: function (el) {
               var _actions = app(state, actions, view, el)
               el._$ = _actions._$
               el._$(props)
               if (_actions.init) _actions.init()
               el._$$ = _actions.uninit
           },
-          onupdate: function (el) {
+          onupdate: function (el) {
               el._$(props)
           },
           onremove: function (el) {
